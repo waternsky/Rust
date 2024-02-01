413c6404926fa53e5bce63736f874a787d5924cf
waternksy <playgamesroyale2@gmail.com>
2024-02-01 19:37:38 +0530

    Chapter 1 & 2 finished guessing game reminded me lot of things.

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,14 +1,10 @@
-use std::io;
-use std::cmp::Ordering;
+use std::{cmp::Ordering, io};
 use rand::Rng;
 
 fn main() {
-    
     println!("Guess the number!");
 
     let secret_number = rand::thread_rng().gen_range(1..=100);
-
-    let mut counter = 0;
 
     loop {
         println!("Please input your guess.");
@@ -17,7 +13,7 @@ fn main() {
 
         io::stdin()
             .read_line(&mut guess)
-            .expect("Unable to read line");
+            .expect("Failed to read line");
 
         let guess: u32 = match guess.trim().parse() {
             Ok(num) => num,
@@ -27,21 +23,12 @@ fn main() {
         println!("You guessed: {guess}");
 
         match guess.cmp(&secret_number) {
-            Ordering::Less => {
-                println!("Too small!");
-                counter = counter + 1;
-            },
-            Ordering::Greater => {
-                println!("Too big!");
-                counter = counter + 1;
-            },
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
             Ordering::Equal => {
                 println!("You win!");
-                println!("Number of attempts: {counter}");
                 break;
-            }
+            },
         }
     }
-    
-
 }
